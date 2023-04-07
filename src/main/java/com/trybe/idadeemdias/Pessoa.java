0097af762b9e515ed536e112e0d55360d525bc73
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-07 17:09:13 -0300

    feat: create method calcularIdadeEmDias

--- src/main/java/com/trybe/idadeemdias/Pessoa.java
+++ src/main/java/com/trybe/idadeemdias/Pessoa.java
@@ -1,7 +1,17 @@
 package com.trybe.idadeemdias;
 
+/**
+ * Class Pessoa.
+ */
 public class Pessoa {
-	
 
-	
+  /**
+   * Method show age to days.
+   */
+  public int calcularIdadeEmDias(int anos, int meses, int dias) {
+    int anosEmDias = anos * 365;
+    int mesesEmDias = meses * 30;
+
+    return anosEmDias + mesesEmDias + dias;
+  }
 }
