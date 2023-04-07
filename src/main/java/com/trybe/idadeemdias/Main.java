a8081e87f0a6b3d32f05c5b95fe5081dc5eaeba2
¨Matheus¨ <ymatheus.h@gmail.com>
2023-04-07 17:10:12 -0300

    feat: input scanner questions and show age in days

--- src/main/java/com/trybe/idadeemdias/Main.java
+++ src/main/java/com/trybe/idadeemdias/Main.java
@@ -2,12 +2,49 @@ package com.trybe.idadeemdias;
 
 import java.util.Scanner;
 
+/**
+ * Class Main.
+ */
 public class Main {
 
-	public static void main(String[] args) {
-		
+  /**
+   * Initial Method.
+   */
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-		
-	}
+    System.out.println("Qual é o seu nome?");
+
+    String name = scanner.next();
+
+    System.out.println("Qual é sua idade em anos, meses e dias?");
+
+    try {
+      System.out.println("anos:");
+
+      int years = scanner.nextInt();
+
+      System.out.println("meses:");
+
+      int months = scanner.nextInt();
+
+      System.out.println("dias:");
+
+      int days = scanner.nextInt();
+
+      Pessoa pessoa = new Pessoa();
+      int age = pessoa.calcularIdadeEmDias(years, months, days);
+
+      StringBuilder sb = new StringBuilder();
+
+      sb.append("Olá ").append(name).append(", sua idade em dias é ").append(age);
+
+      System.out.println(sb.toString());
+    } catch (Exception e) {
+      System.out.println("Entre com uma opção válida!");
+    }
+
+    scanner.close();
+  }
 
 }
